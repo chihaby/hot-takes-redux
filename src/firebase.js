f25ed77f8e593ac9f9ece94238496ce8bfb56510
Rad <chihaby@gmail.com>
2019-11-19 10:04:21 -0800

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyAhWMYWhBG2UvsNMZ-qVLbaqVZ5aTzwsNU',
-  authDomain: 'hot-takes.firebaseapp.com',
-  databaseURL: 'https://hot-takes.firebaseio.com',
-  storageBucket: 'hot-takes.appspot.com',
-  messagingSenderId: '823384132241'
+  apiKey: "AIzaSyCYkFsBI74gBqQ3qX_pfhS6JQCqxk2y3yY",
+  authDomain: "first-flight-6df89.firebaseapp.com",
+  databaseURL: "https://first-flight-6df89.firebaseio.com",
+  projectId: "first-flight-6df89",
+  storageBucket: "first-flight-6df89.appspot.com",
+  messagingSenderId: "335955122918",
+  appId: "1:335955122918:web:9a8807d017f3787fb4f927",
+  measurementId: "G-KR15X6JR68"
 };
 
 firebase.initializeApp(config);
